2736ea61702f9783079531d049ddbdad9b01df53
beenbin <77373566+showme0241@users.noreply.github.com>
2023-07-16 03:50:00 +0900

    :notebook: react : msw > 로그인 로직 구성중
    p.s 응닶값이 정상일 때, 유저id에 저장된 정보를 set하고 있어야한다....

---
+++ chapter_react/src/chapter03/Pages/Sign/SignIn.js
@@ -0,0 +1,107 @@
+import styled from "@emotion/styled";
+import { Controller, useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Button from "../../Components/Button/Button";
+import { validationRules } from "../../Consts/validationRules";
+import { axiosInstance } from "../../__mocks__/Apis/@core";
+import ControlItem from "./Components/ControlItem";
+
+const PATH = "/api/user/login";
+
+export default function SignIn() {
+    const { watch, control, handleSubmit } = useForm();
+    const navigate = useNavigate();
+
+    const email = watch("email");
+    const pw = watch("pw");
+
+    const onSubmitHandler = async (data) => {
+        const { email, pw } = data;
+
+        try {
+            const res = await axiosInstance.post(PATH, { email, pw });
+
+            if (res.status === "200") {
+                // 로그인 정보 저장
+                // localStorage.setItem("고유키", 저장값)
+
+                navigate("/");
+            }
+        } catch (error) {
+            console.log("");
+        }
+    };
+
+    return (
+        <S.Wrapper>
+            <S.Container>
+                <h5>리액트 훅폼 : 로그인</h5>
+                <form onSubmit={handleSubmit(onSubmitHandler)}>
+                    <Controller
+                        name="email"
+                        control={control}
+                        rules={validationRules.email}
+                        render={({ field, fieldState: { error } }) => {
+                            return (
+                                <ControlItem
+                                    field={field}
+                                    error={error?.message}
+                                />
+                            );
+                        }}
+                    />
+                    <Controller
+                        name="pw"
+                        control={control}
+                        rules={validationRules.pw}
+                        render={({ field, fieldState: { error } }) => {
+                            return (
+                                <ControlItem
+                                    field={field}
+                                    error={error?.message}
+                                />
+                            );
+                        }}
+                    />
+                    <S.StyledButton
+                        size="default"
+                        color="default"
+                        shape="default"
+                        type="submit"
+                        disabled={email === "" || pw === "" ? true : false}
+                    >
+                        로그인
+                    </S.StyledButton>
+                </form>
+            </S.Container>
+        </S.Wrapper>
+    );
+}
+
+const Wrapper = styled.div`
+    width: 90%;
+    margin: 0 auto;
+    margin-top: 80px;
+`;
+
+const Container = styled.div`
+    & > h5 {
+        margin-bottom: 40px;
+    }
+
+    & > form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+`;
+
+const StyledButton = styled(Button)`
+    margin-top: 40px;
+`;
+
+const S = {
+    Wrapper,
+    Container,
+    StyledButton,
+};
