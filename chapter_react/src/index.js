8b504b0f57ea9c2153605f9afa14a0a62e0c42d5
beenbin <77373566+showme0241@users.noreply.github.com>
2023-07-16 03:04:56 +0900

    :notebook: react : chapter03의 msw 실행를 위한 설치 및 세팅

--- chapter_react/src/index.js
+++ chapter_react/src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import worker from "./chapter03/__mocks__/browser";
+
+const env = process.env;
+
+if (env.NODE_ENV === "development") {
+    worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
