18c9ce0db69932f450ab7e431961f65632555328
beenbin <showme0241@gmail.com>
2023-08-06 18:19:05 +0900

    :notebook_with_decorative_cover: Next : 서버 컴포넌트와 클라이언트 컴포넌트
    본편에서 서버와 클라이언트간 사용되는 컴포넌트의 분리 로직은 next의 주요 특징이 아니라 react 18버전에서 업데이트된 새로운 아키텍쳐이다.
    
    next JS에서 캐싱된 데이터를 패칭하고 유효 시간을 간편하게 관리할 수 있도록 구성되어있다. (공식문서 추가참조)
    
    학습하면서 느끼는 건 react에서의 불편한 점이 많은 부분 보완되었고, 특히 라우팅 관련된 구조가 많이 다르다. (마지막으로 넥스트 ... ㅎㄷㄷ하고 어렵다고 미뤘던 것이 약간 후회쓰... ㅎㅎ)

--- lecture_next/src/app/create/page.js
+++ lecture_next/src/app/create/page.js
@@ -1,3 +1,42 @@
+"use client";
+
+// next 12버전에서는 next/router에 useRouter를 import한다.
+import { useRouter } from "next/navigation";
+
 export default function Create() {
-    return <>Create!!</>;
+    const router = useRouter();
+
+    return (
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                const title = e.target.title.value;
+                const body = e.target.body.value;
+                const options = {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ title, body }),
+                };
+
+                fetch(`http://localhost:9999/topics`, options)
+                    .then((res) => res.json())
+                    .then((result) => {
+                        router.refresh();
+                        router.push(`/read/${result.id}`);
+                    });
+            }}
+        >
+            <p>
+                <input type="text" name="title" placeholder="title" />
+            </p>
+            <p>
+                <textarea name="body" placeholder="body" />
+            </p>
+            <p>
+                <input type="submit" value="create" />
+            </p>
+        </form>
+    );
 }
