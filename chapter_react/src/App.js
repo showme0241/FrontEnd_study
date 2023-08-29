3fbb79425275a2c12262fadcae85d2e7cbff3d8e
beenbin <showme0241@gmail.com>
2023-08-30 01:17:24 +0900

    :notebook: React : yup 라이브러리 사용을 통한 FormElement의 커스텀 보강
    1. useController 훅함수를 통해 FormElement의 커스텀 및 코드 사용량 감소
    
    2. yup 라이브러리 사용으로 rules로 일일이 정규식으로 핸들링해야하는 번거로움 감소
    
    => 전반적으로 코드양이 줄다보니 가독성이 좋아지고 코드를 해독하기에도 명확한 관심사 분리가 이루어져 편리해졌다.

--- chapter_react/src/App.js
+++ chapter_react/src/App.js
@@ -1,7 +1,40 @@
+import { yupResolver } from "@hookform/resolvers/yup";
 import React from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import FormElement from "./chapter06/FormElement";
+
+const schema = yup.object({
+    email: yup
+        .string()
+        .email("이메일 형식이 아닙니다.")
+        .required("이메일을 입력해주세요."),
+    password: yup
+        .string()
+        .required("비밀번호를 입력해 주세요")
+        .matches(
+            /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@!%*#?&])[A-Za-z\d@!%*#?&]{8,}$/,
+            "8글자 이상 영문자, 숫자, 특수문자를 조합해서 입력하세요"
+        ),
+});
 
 function App() {
-    return <div>yup test 준비</div>;
+    const { control, handleSubmit } = useForm({
+        resolver: yupResolver(schema),
+        defaultValues: { email: "", password: "" },
+    });
+
+    const onSubmitHandler = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
+            <FormElement control={control} name="email" label="이메일 계정" />
+            <FormElement control={control} name="password" label="비밀번호" />
+            <button type="submit">제출</button>
+        </form>
+    );
 }
 
 export default App;
--- chapter_react/src/chapter06/FormElement.jsx
+++ chapter_react/src/chapter06/FormElement.jsx
@@ -1,3 +1,21 @@
-const FormElement = () => {};
+import { useController } from "react-hook-form";
+
+const FormElement = ({ control, name, label }) => {
+    const {
+        field,
+        fieldState: { error: errors },
+    } = useController({ control, name });
+
+    return (
+        <>
+            <label>{label}</label>
+            <input
+                type={name === "password" ? "password" : "text"}
+                {...field}
+            />
+            {errors && <p>{errors.message}</p>}
+        </>
+    );
+};
 
 export default FormElement;
